71bf9e93f0369bdcef7db67e5e41591ffb0db7d8
wanghua <wanghua@kuaishou.com>
2023-09-27 19:35:00 +0800

    go cmd

---
+++ cli/DBdriver/dbdriver.go
@@ -0,0 +1,137 @@
+// sqlite3 驱动封装
+// joshua
+// 2023.9.21
+
+package DBdriver
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const dbfile string = "/Users/joshua/repo/github.com/MovieExcavator/data/movies.db"
+
+func CheckImportOk() string {
+	return "ok"
+}
+func getConn() *sql.DB {
+	db, err := sql.Open("sqlite3", dbfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func queryRowsAndPrint(sql_string string) {
+	db := getConn()
+	defer db.Close()
+
+	rows, err := db.Query(sql_string)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var hash, title, tags, create_time string
+		var score float64
+		err := rows.Scan(&hash, &title, &tags, &score, &create_time)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s | %-30s | %-.2f|%s| %s\n", hash, title, score, create_time, tags)
+	}
+
+}
+
+func QueryStatusCount() {
+	sql_string := "select status, count(status) from movie group by status;"
+	db := getConn()
+	defer db.Close()
+
+	var count_like int
+	var count_unlike int
+	var count_downloading int
+	var count_downloaded int
+	var total int
+
+	rows, err := db.Query(sql_string)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var status, count int
+		err := rows.Scan(&status, &count)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch status {
+		case 1:
+			count_unlike = count
+		case 2:
+			count_like = count
+		case 3:
+			count_downloading = count
+		case 4:
+			count_downloaded = count
+		}
+	}
+	total = count_like + count_unlike + count_downloading + count_downloaded
+
+	fmt.Println("Total:", total)
+	fmt.Println("喜欢的电影:", count_like)
+	fmt.Println("忽略的电影:", count_unlike)
+	fmt.Println("下载中:", count_downloading)
+	fmt.Println("下载完成:", count_downloaded)
+}
+
+func ShowDownloadingMovies() {
+	sql_string := "select hash, title, tags, score, create_time from movie where status = 3;"
+	queryRowsAndPrint(sql_string)
+}
+
+func ShowUnlinkedMovies(limit int) {
+	sql_temp := "select hash, title, tags, score, create_time from movie where status = 1  order by create_time desc limit %d;"
+	sql_string := fmt.Sprintf(sql_temp, limit)
+	queryRowsAndPrint(sql_string)
+}
+
+func ShowDoneMovies(limit int) {
+	sql_temp := "select hash, title, tags, score, create_time from movie where status = 4 order by create_time desc limit %d;"
+	sql_string := fmt.Sprintf(sql_temp, limit)
+	queryRowsAndPrint(sql_string)
+}
+
+func SetStatus(hashes []string, status int) {
+	var placeholder string
+	for i := 0; i < len(hashes); i++ {
+		if i == 0 {
+			placeholder += "?"
+		} else {
+			placeholder += ", ?"
+		}
+	}
+	sql_temp := "update movie set status = ? where hash in (%s);"
+	sql_temp_with_placeholder := fmt.Sprintf(sql_temp, placeholder)
+
+	values := make([]interface{}, 0, 6)
+	values = append(values, status)
+	for _, v := range hashes {
+		values = append(values, v)
+	}
+
+	db := getConn()
+	defer db.Close()
+	res, err := db.Exec(sql_temp_with_placeholder, values...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	n, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("成功设置%d条数据\n", n)
+}
---
+++ cli/main.go
@@ -0,0 +1,88 @@
+// 命令行交互
+// joshua
+// 2023.9.21
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"moviecli/DBdriver"
+	"os"
+)
+
+func showDownloadingMovies(isShow bool) {
+	if isShow {
+		fmt.Println("=== 下载中的电影 ===")
+		DBdriver.ShowDownloadingMovies()
+	}
+}
+
+func showUnlikedMovies(limit int) {
+	if limit > 0 {
+		fmt.Println("=== 标记忽略 ===")
+		DBdriver.ShowUnlinkedMovies(limit)
+	}
+}
+
+func showDoneMovies(limit int) {
+	if limit > 0 {
+		fmt.Println("=== 下载完成 ===")
+		DBdriver.ShowDoneMovies(limit)
+	}
+}
+
+func setStatus(hashes []string, status int) {
+	L := 5
+	if n := len(hashes); n > L || n < 1 {
+		fmt.Printf("一次至少一个或最多设置%d个!\n", L)
+		os.Exit(0)
+	}
+	if status > 4 {
+		fmt.Printf("dest_status 取值范围为 0~4, 输入: %d\n", status)
+		os.Exit(0)
+	}
+	DBdriver.SetStatus(hashes, status)
+}
+
+func main() {
+	// 子命令 show
+	showCmd := flag.NewFlagSet("show", flag.ExitOnError)
+	var isShowDownloading bool
+	var limit int
+	var showDoneBylimit int
+	showCmd.BoolVar(&isShowDownloading, "d", false, "Is show all downloading movies")
+	showCmd.IntVar(&limit, "i", 0, "show the unliked movies with last [n] rows order by create_time")
+	showCmd.IntVar(&showDoneBylimit, "w", 0, "show the downloaded movies with last [n] rows order by create_time")
+
+	// 子命令 reset
+	resetCmd := flag.NewFlagSet("reset", flag.ExitOnError)
+	var dest_status int
+	resetCmd.IntVar(&dest_status, "dst", 2, "reset the status to [dst] with special hash of movie, default 1 (1 unlike, 2 like, 3 downloading, 4 done)")
+
+	if len(os.Args) < 2 {
+		DBdriver.QueryStatusCount()
+		os.Exit(0)
+	}
+
+	switch os.Args[1] {
+	case "show":
+		showCmd.Parse(os.Args[2:])
+		DBdriver.QueryStatusCount()
+		// -d
+		showDownloadingMovies(isShowDownloading)
+		// -i
+		showUnlikedMovies(limit)
+		// -w
+		showDoneMovies(showDoneBylimit)
+	case "reset":
+		resetCmd.Parse(os.Args[2:])
+		setStatus(resetCmd.Args(), dest_status)
+		// reset status
+	case "-h", "--help":
+		showCmd.Usage()
+	default:
+		fmt.Println("Unknow cmd:", os.Args[1])
+		showCmd.Usage()
+	}
+}
